4bb0337f894ad0009447753646183da803cf33cd
wangshuailong <oopswangsl@gmail.com>
2016-01-14 21:58:25 +0800

    update

--- book/collective_intelligence/ch08/numpredict.py
+++ book/collective_intelligence/ch08/numpredict.py
@@ -63,3 +63,59 @@ def knnestimate(data, vec1, k = 3):
     avg = avg/k
 
     return avg
+
+def inverseweight(dist, num = 1.0, const = 0.1):
+    return num/(dist + const)
+
+def subtractweight(dist, const = 1.0):
+    if dist > const:
+        return 0
+    else:
+        return const - dist
+
+def gaussian(dist, sigma = 10.0):
+    return math.e**(-dist**2/(2*sigma**2))
+    
+    
+def weightknn(data, vec1, k = 5, weightf = gaussian):
+    # Get distances
+    dlist = getdistance(data, vec1)
+    avg = 0.0
+    totalweight = 0.0
+
+    # Get weighted average
+    for i in range(k):
+        dist = dlist[i][0]
+        idx = dlist[i][1]
+        weight = weightf(dist)
+
+        avg += weight * data[idx]['result']
+        totalweight += weight
+    avg = avg/totalweight
+
+    return avg
+
+
+def dividedata(data, test = 0.05):
+    trainset = []
+    testset = []
+    for row in data:
+        if random() < test:
+            testset.append(row)
+        else:
+            trainset.append(row)
+    return trainset, testset
+
+def testalgorithm(algf, trainset, testset):
+    error = 0.0
+    for row in testset:
+        guess = algf(trainset, row['input'])
+        error += (row['result'] - guess)**2
+    return error/len(testset)
+
+def crossvalidate(algf, data, trials = 100, test = 0.05):
+    error = 0.0
+    for i in range(trials):
+        trainset, testset = dividedata(data, test)
+        error += testalgorithm(algf, trainset, testset)
+    return error/trials
